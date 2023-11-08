a1c4be635aa929d6c42a15113b62f257b9d8ee7b
Amatikay <sengle060@gmail.com>
2023-11-09 00:48:22 +0700

    update lab 1

--- Lab-1_2023-24/main.r
+++ Lab-1_2023-24/main.r
@@ -1,3 +1,11 @@
+#library(moments)
+#Mode function
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+
 
 #Poisson distribution
 poisson_sample <- rpois(200, 5)  # Генерируем выборку с распределением Пуассона
@@ -9,22 +17,31 @@ sorted_poisson_sample <- sort(poisson_sample)
 freq_table <- table(sorted_poisson_sample)
 relative_frequencies <- prop.table(freq_table, margin = NULL)
 
-
-
 #Create variation series
 variation_series <- data.frame(
   Значение = as.numeric(names(freq_table)),
   Абсолютная_частота = as.numeric(freq_table),
-  Относительная_частота = relative_frequencies[1]
+  Относительная_частота = as.numeric(relative_frequencies)
 )
 
-cat("Ряд абсолютн и оносительных частот")
+cat("Ряд абсолютн и оносительных частот\n")
 print(variation_series)
 
 
 #Frequency Polygon
-x11()
 sample_hist <- hist(poisson_sample)
 lines(sample_hist$counts ~ sample_hist$mids)
 
-end <- readline(prompt = "Enter for end program")
+#Estimates of statistics
+cat("Числовые характеристики\n")
+mean(poisson_sample) # Математическое ожидание
+var(poisson_sample) # Дисперсия
+sd(poisson_sample) 	# Стандартное отклониение
+median(poisson_sample) # Медиана
+quantile(poisson_sample, probs = 0.5) # Квантили
+Mode(poisson_sample)
+
+#Асиметрия и эксцесс нужна библиотека моментов, но она не подключается почему то
+#skewness(poisson_sample)
+#kurtosis(poisson_sample)
+
